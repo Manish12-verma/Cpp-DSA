206dfe0e452000c1d83ab3adcd77bb22dc4e2517
Manish verma <110876979+Manish12-verma@users.noreply.github.com>
2024-01-14 00:03:46 +0530

    Add files via upload

---
+++ Function/prime-bt-range.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h> 
+#include <iostream>
+
+using namespace std;
+
+//Write your totalPrime function here
+bool isPrime(int n){
+
+    if(n <= 1){
+
+        return false;
+
+    }
+
+    for(int i = 2; i <= sqrt(n); i++){
+
+        if(n%i == 0){
+
+            return false;
+
+        }
+
+    }
+
+    return true;
+}
+int totalPrime(int S,int E){
+      int count = 0;
+
+    for(int i = S; i <= E ; i++){
+
+        if(isPrime(i)){
+
+            count++;
+
+        }
+
+    }
+
+    return count;
+}
+
+int main() {
+    int S, E;
+    cin >> S >> E;
+    cout << totalPrime(S, E);
+    return 0;
+}
