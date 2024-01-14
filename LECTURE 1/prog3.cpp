0dd14c19a178b3860c6087138e66b8709a98461a
Manish verma <110876979+Manish12-verma@users.noreply.github.com>
2024-01-14 20:09:45 +0530

    Add files via upload

---
+++ LECTURE 1/prog3.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int x=10;
+    int y= (x++,x++,x++);
+    cout << x<< " "<<y;
+    return 0;
+}
---
+++ LECTURE 1/prog4.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int x=3;
+    if(x==2); x=0;
+    if (x==3) x++;
+    else x+=2;
+
+    cout<< "x= "<<x;
+
+    return 0;
+}
