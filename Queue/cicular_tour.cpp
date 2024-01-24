6bd4b4b9bfff1dac7191d432e01d7502b6a72a85
Manish verma <110876979+Manish12-verma@users.noreply.github.com>
2024-01-24 10:32:26 +0530

    Create cicular_tour.cpp
    Suppose there is a circle. There are N petrol pumps on that circle. You will be given two sets of data.
    1. The amount of petrol that every petrol pump has.
    2. Distance from that petrol pump to the next petrol pump.
    Find a starting point where the truck can start to get through the complete circle without exhausting its petrol in between.
    Note :  Assume for 1 litre petrol, the truck can go 1 unit of distance.

---
+++ Queue/cicular_tour.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h> 
+int firstCircularTour(vector<int>& petrol, vector<int>& distance, int N)
+{
+	// write your code here
+	 int deficit = 0;
+       int balance =0;
+       int start =0;
+       for(int i=0;i<N;i++){
+           balance +=petrol[i]-distance[i];
+           if(balance<0){
+               deficit +=balance;
+               start =i+1;
+               balance = 0;
+           }
+       }
+       if(deficit+balance>=0)
+         return start;
+       else
+         return -1;
+}
