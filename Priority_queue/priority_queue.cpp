c9d3ee7fd30b742152a4802cbba896c1e063013c
Manish verma <110876979+Manish12-verma@users.noreply.github.com>
2024-06-30 09:43:10 +0530

    updated prority_queue

--- Priority_queue/priority_queue.cpp
+++ Priority_queue/priority_queue.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 void mySort(int arr[],int n){
     priority_queue<int> pq(arr,arr+n);
-    for(int i=0;i<n;i++){
+    for(int i=n-1;i>=0;i--){
         arr[i] = pq.top();
         cout<< arr[i]<<" ";
         pq.pop();
